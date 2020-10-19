ec9d48bd798bd075772c63be3d830b36e12d460f
Luiz <luiz.felipe.556@outlook.com>
2020-10-19 20:24:15 -0300

    Commit 18

--- src/main/java/HtmlStatement.java
+++ src/main/java/HtmlStatement.java
@@ -2,17 +2,6 @@ import java.util.Enumeration;
 
 public class HtmlStatement extends Statement {
    
-    public String value(Customer aCustomer) {
-      Enumeration rentals = aCustomer.getRentals(); 
-      String result = valueOne(aCustomer);
-      while (rentals.hasMoreElements()) {
-         Rental each = (Rental) rentals.nextElement(); 
-         result += valueTwo(each);
-      }
-      result += valueThree(aCustomer);
-      return result;
-   }
-   
     public String valueOne(Customer aCustomer){
 	String result = "<H1>Rentals for <EM>" + aCustomer.getName() + "</EM></H1><P>\n";
         return result;
--- src/main/java/Statement.java
+++ src/main/java/Statement.java
@@ -1,3 +1,18 @@
-public class Statement {
+import java.util.Enumeration;
 
+public abstract class Statement {
+    public String value(Customer aCustomer) {
+      Enumeration rentals = aCustomer.getRentals(); 
+      String result = valueOne(aCustomer);
+      while (rentals.hasMoreElements()) {
+         Rental each = (Rental) rentals.nextElement(); 
+         result += valueTwo(each);
+      }
+      result += valueThree(aCustomer);
+      return result;
+   }
+    
+    abstract String valueOne(Customer aCustomer);
+    abstract String valueTwo(Rental each);
+    abstract String valueThree(Customer aCustomer);
 }
